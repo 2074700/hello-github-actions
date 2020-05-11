1ae3056607253a0d80629bfd2b71e56dc2c93246
2074700 <65171221+2074700@users.noreply.github.com>
2020-05-11 21:26:29 +0800

    Create lyflib.cpp

---
+++ lyflib.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define LYFTRUE -1
+#define LYFFALSE 0
+
+const short int left = 1;
+const short int right = 2;
+
+#include "tree.cpp"
+#include "findString.cpp"
+#include "heatSort.cpp"
+#include "countingSort.cpp"
+#include "lyfxml.h"
